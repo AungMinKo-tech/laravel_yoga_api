dba01535f964f4f324fdaca71f39ff4c33ca5a4c
ThawHtooZin <thawhtooin@gmail.com>
2025-09-30 21:12:16 +0630

    Added all Subscriptions & Payments (Dev 3) Endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Dashboard\UserController;
+use App\Http\Controllers\Api\SubscriptionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,13 +12,17 @@ Route::post('/login', [AuthController::class, 'login']);
 //Trainer
 Route::middleware(['auth:sanctum', 'trainerMiddleware'])->group(function () {
     //user
-
+    Route::get('/subscriptions', [SubscriptionController::class, 'index']);
 
     //Admin
     Route::middleware(['auth:sanctum', 'adminMiddleware'])->group(function () {
         //user
         Route::resource('users', UserController::class)->only('index', 'store');
-
+        Route::post('/subscriptions', [SubscriptionController::class, 'store']);
+        Route::post('/subscriptions/{id}', [SubscriptionController::class, 'update']);
+        Route::delete('/subscriptions/{id}', [SubscriptionController::class, 'destroy']);
+        Route::post('/users/{id}/subscriptions', [SubscriptionController::class, 'assignSubscription']);
+        Route::get('/users/{id}/subscriptions', [SubscriptionController::class, 'getSubscription']);
     });
 
 });
